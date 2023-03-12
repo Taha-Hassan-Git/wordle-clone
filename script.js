f43d97e847f43760b5d01f457a43119f1f8d896b
Taha Hassan <tahafhassan@gmail.com>
2023-03-12 12:09:22 +0000

    user is alerted when not enough letters

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 document.querySelectorAll('.key').forEach(key => key.addEventListener('click', handleClick));
-window.addEventListener('keydown', handleKey)
+window.addEventListener('keydown', handleKeyBoard)
 const textbox = document.querySelector('.input')
 
-function handleKey(e){
+function handleKeyBoard(e){
     if (e.key === 'Enter'){return handleSubmit()}
     if (e.key === 'Backspace'){return handleBackspace()}
-    else if (e.code.startsWith('Key') ) {
-        return handleLetter(e.key.toUpperCase())}
+    else if (e.code.startsWith('Key') ) {return handleLetter(e.key.toUpperCase())}
 }
 
 function handleClick(e){
@@ -23,7 +22,7 @@ function handleLetter(letter){
 function handleSubmit(){
     const textbox = document.querySelector('.input')
     if (textbox.value.length >4) {textbox.value = '';}
-    else (console.log('not long enough'));
+    else (alert('Not enough letters'));
 }
 function handleBackspace(){
     const textbox = document.querySelector('.input')
